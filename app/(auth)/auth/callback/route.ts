0bd7205bd349f7886fc8cc594d6dc31b5549e0cd
luth <88308177+luthpg@users.noreply.github.com>
2025-06-04 05:03:22 +0000

    update routes

--- app/(auth)/auth/callback/route.ts
+++ app/(auth)/auth/callback/route.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@/lib/supabase/server';
+import { NextDataPathnameNormalizer } from 'next/dist/server/normalizers/request/next-data';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
@@ -10,6 +11,9 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(`${requestUrl.origin}/dashboard`);
+    } else {
+      console.warn(error);
+      return NextResponse.redirect(`${requestUrl.origin}/login`)
     }
   }
 
